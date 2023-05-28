f6f50c51aa2dc6146f5e5fb39c3b03aa05e0124a
munisekhar1434 <kuntumallamunisekhar2000@gmail.com>
2023-05-28 16:54:00 +0530

    changes

--- src/Redux/reducer.js
+++ src/Redux/reducer.js
@@ -16,13 +16,13 @@ export default function reducer(state = initialState, action) {
 
       let flag=0
        if(action.payload.first_name==""||action.payload.last_name==""||action.payload.mob==""){
-          alert('ohh You Missed Required Input , Please fill')
+          alert('Please fill form')
         flag=1
       }
       else{
      state.contacts.forEach((el)=>{
         if(el.first_name==action.payload.first_name&&el.last_name==action.payload.last_name){
-            alert('Name Already Exist In Contact')
+            alert('Name Already given')
             flag=1
         }
       
@@ -31,7 +31,7 @@ export default function reducer(state = initialState, action) {
  
 
       if(!flag){
-        alert('Contact Saved Successfully!!!')
+        alert('Contact Save done')
        
         let updatedContacts=JSON.parse(localStorage.getItem("contacts"))||[]
         updatedContacts.push({id:state.contacts.length+1,...action.payload})
@@ -63,7 +63,7 @@ export default function reducer(state = initialState, action) {
     case EDIT_CONTACT: {
 
       if(action.payload.first_name==""||action.payload.last_name==""||action.payload.mob==""){
-        alert('Input Fields Can Not Be Leave Empty')
+        alert('please fill the form')
         return state
       }
 
@@ -75,7 +75,7 @@ export default function reducer(state = initialState, action) {
 
         Contacts.forEach((el)=>{
           if(el.id!=action.payload.id&&el.first_name==action.payload.first_name&&el.last_name==action.payload.last_name){
-            alert("Name Already Exist!!")
+            alert("Name Already added")
             flag=1
             return state
           }
@@ -94,14 +94,13 @@ export default function reducer(state = initialState, action) {
           }
         })
         localStorage.setItem("contacts",JSON.stringify(updatedContacts))
-        alert('Contact has been Updated')
+        alert('Contact edit done.')
            return {
         ...state,
         contacts: state.contacts.map((el)=>{
           if(el.id==action.payload.id){
              return  el={...action.payload}
 
-            return el={...action.payload}
           }
           else{
             return el
